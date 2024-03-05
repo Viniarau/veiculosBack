8b6c74ebbf53a2751265b151dccde4c16e707dd3
Viniarau <vinicius.j.s.a22@hotmail.com>
2024-03-05 16:04:40 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); 
+    }
+    next(); 
+});
+
+app.get('/vehicles', (req, res) => {
+    fs.readFile('./data/vehicles.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
